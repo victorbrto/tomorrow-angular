f326ad0174cf18c712b9f3d8b8cbf20fe3858a75
victorbrto <victorbrito1103@gmail.com>
2025-06-11 11:54:09 -0300

    aula 1106

--- escola-app/src/app/app.component.ts
+++ escola-app/src/app/app.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListaDeDisciplinas } from './lista-de-disciplinas/lista-de-disciplinas';
+import { CommonModule } from '@angular/common';
+import { Disciplina } from './disciplina.model';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ListaDeDisciplinas, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'escola-app';
+  selecionado = null;
+  disciplinas = [
+    new Disciplina('Língua Portuguesa', 'O objetivo é...'),
+    new Disciplina('Educação Física', 'O objetivo é...')
+  ]
+
+  selecionar(disciplina: null) {
+    this.selecionado = disciplina;
+  }
 }
---
+++ escola-app/src/app/disciplina.model.spec.ts
@@ -0,0 +1,7 @@
+import { Disciplina } from './disciplina.model';
+
+describe('Disciplina', () => {
+  it('should create an instance', () => {
+    expect(new Disciplina()).toBeTruthy();
+  });
+});
