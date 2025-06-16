8edf020d911116e35e11bfca8ef4b94d1f012125
victorbrto <victorbrito1103@gmail.com>
2025-06-15 21:23:15 -0300

    aula 1306 2

--- escola-app/src/app/lista-de-disciplinas/lista-de-disciplinas.ts
+++ escola-app/src/app/lista-de-disciplinas/lista-de-disciplinas.ts
@@ -1,9 +1,10 @@
 import { Component, EventEmitter, input, Input, OnInit, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Disciplina } from '../disciplina.model';
 
 @Component({
   selector: 'app-lista-de-disciplinas',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './lista-de-disciplinas.html',
   styleUrl: './lista-de-disciplinas.css'
 })
@@ -13,10 +14,35 @@ export class ListaDeDisciplinas {
   disciplinas = [new Disciplina(" ", " ")];
 
   @Input()
-  editando = null;
+  editando: Disciplina | null = null;
+
+  @Input()
+  selecionado: null | Disciplina = null
 
   @Output()
   onEditar = new EventEmitter<Disciplina>();
 
+  @Output()
+  onExcluir = new EventEmitter<Disciplina>();
 
+  @Output()
+  onSelecionar = new EventEmitter<Disciplina>();
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  excluir(disciplina: Disciplina) {
+    this.onExcluir.emit(disciplina);
+  }
+
+  editar(disciplina: Disciplina) {
+    this.onEditar.emit(disciplina);
+  }
+
+  selecionar(disciplina: Disciplina) {
+    this.onSelecionar.emit(disciplina);
+  }
 }
